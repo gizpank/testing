87c8cabe2db037b7c6eed9ff20d183eab26d7bac
gizpank <gizpank@mail.ru>
2017-11-26 21:40:50 +0400

    01

--- com.pflb.testingGitOperations/src/testingFile1.java
+++ com.pflb.testingGitOperations/src/testingFile1.java
@@ -1,11 +1,3 @@
 public class testingFile1 {
-    String number1;
-    String number2;
-    String number3;
-<<<<<<<<< Temporary merge branch 1
-    String number5;
-=========
-    String number4;
-    int numb06;
->>>>>>>>> Temporary merge branch 2
+    Integer i1;
 }
